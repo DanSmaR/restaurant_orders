fc2154d9b29151e534824b3b3812df1baa3c250a
Danilo Martins <dansm86@outlook.com>
2023-04-24 08:56:32 -0300

    feat:req01/test ingredient class

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1 - initial commit
 def test_ingredient():
-    pass
+    farinha = Ingredient("farinha")
+    farinha2 = Ingredient("farinha")
+    ovo = Ingredient("ovo")
+
+    farinha_hash = hash(farinha)
+    assert farinha_hash == hash(farinha2)
+    assert hash(ovo) != farinha_hash
+
+    assert farinha2 == farinha
+    assert ovo != farinha
+
+    assert repr(farinha) == "Ingredient('farinha')"
+
+    assert farinha.name == "farinha"
+    assert farinha2.name == farinha.name
+
+    assert farinha.restrictions == {Restriction.GLUTEN}
+    assert ovo.restrictions == {Restriction.ANIMAL_DERIVED}
